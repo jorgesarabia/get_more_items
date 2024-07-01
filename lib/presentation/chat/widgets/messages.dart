201ef6960f37f3905f4d2320ce8fcc7eaeefff7f
jorgesarabia <jorge@sarabiajor.ge>
2024-07-01 17:26:29 -0300

    scroll to bottom

--- lib/presentation/chat/widgets/messages.dart
+++ lib/presentation/chat/widgets/messages.dart
@@ -13,21 +13,40 @@ class _MessagesState extends State<_Messages> {
   List<MessageEntity> _messages = [];
   ScrollController? _scrollController = ScrollController();
   bool _isGettingMoreMessages = false;
+  int _lastLength = 0;
+  bool _mustNotJumpToBottom = false;
   late final MessageRepository _messageRepository;
 
   @override
   void initState() {
     _messageRepository = widget.repository;
     _scrollController?.addListener(_loadMoreMessages);
+    _scrollController?.addListener(_setScrollToBottom);
     super.initState();
   }
 
   @override
   void dispose() {
     _scrollController?.removeListener(_loadMoreMessages);
+    _scrollController?.removeListener(_setScrollToBottom);
     _scrollController?.dispose();
     _scrollController = null;
     super.dispose();
+  }
+
+  void _scrollTo([int durationInMilliseconds = 200]) {
+    Future.delayed(Duration(milliseconds: durationInMilliseconds)).then((_) {
+      if (_mustNotJumpToBottom) return;
+      _scrollController?.jumpTo(_scrollController?.position.maxScrollExtent ?? 0.0);
+    });
+  }
+
+  void _setScrollToBottom() {
+    final position = _scrollController?.position;
+    if (position == null) return;
+    if (position.pixels == position.maxScrollExtent) {
+      _mustNotJumpToBottom = false;
+    }
   }
 
   Future<void> _loadMoreMessages() async {
@@ -38,9 +57,11 @@ class _MessagesState extends State<_Messages> {
 
     setState(() => _isGettingMoreMessages = true);
 
+    _mustNotJumpToBottom = true;
     await _messageRepository.loadMoreMessages();
 
     setState(() => _isGettingMoreMessages = false);
+    // WidgetsBinding.instance.addPostFrameCallback((_) => _scrollTo());
   }
 
   @override
@@ -58,6 +79,11 @@ class _MessagesState extends State<_Messages> {
           controller: _scrollController,
           itemCount: _messages.length,
           itemBuilder: (context, index) {
+            if (_messages.length != _lastLength) {
+              _lastLength = index + 1;
+              _scrollTo();
+            }
+
             return Column(
               mainAxisSize: MainAxisSize.min,
               children: [
